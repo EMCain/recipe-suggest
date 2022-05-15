3a3c672351a7feed527417c9678ba7e338c22a16
EMCain <11471197+EMCain@users.noreply.github.com>
2022-05-15 14:08:08 -0700

    Add content to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>
+      This is a personal project by Emily Cain demonstrating simple Vue
+      concepts.
+    </p>
+    <p>
+      You can learn more on
+      <a href="https://emcain.github.io">my portfolio site</a> or message me on
+      <a href="https://linkedin.com/in/emcain">LinkedIn</a>
+      if you enjoyed this project!
+    </p>
+    Color pallate from
+    <a href="https://colorhunt.co/palette/d1e8e4c37b89bccc9aeae7c6"
+      >Color Hunt</a
+    >
   </div>
 </template>
